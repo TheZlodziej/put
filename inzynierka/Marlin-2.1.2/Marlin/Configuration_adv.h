beff071a3582e017aa865e3bdb6929671b605662
minekuba <galaxykuby@gmail.com>
2024-01-17 10:10:40 +0100

    fix pwm freq for fan

--- inzynierka/Marlin-2.1.2/Marlin/Configuration_adv.h
+++ inzynierka/Marlin-2.1.2/Marlin/Configuration_adv.h
@@ -579,8 +579,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-//#define FAN_MIN_PWM 50 // TODO
-//#define FAN_MAX_PWM 128 // TODO
+#define FAN_MIN_PWM 50 // TODO
+#define FAN_MAX_PWM 255 // TODO
 
 /**
  * Fan Fast PWM
